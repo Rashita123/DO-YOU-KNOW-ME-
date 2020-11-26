a1e6e8238f0b7903b0f6958e4d7104314b1ac484
Rashita Mehta <mehta.rashita18f@gmail.com>
2020-11-26 16:19:41 +0000

    welcome message

---
+++ index.js
@@ -0,0 +1,6 @@
+var readlineSync=require('readline-sync');
+
+var name=readlineSync.question('Hey! Whats your name? ');
+console.log('Welcome '+name+' to /DO YOU KNOW Rashita/- Game!');
+console.log("lets Play!");
+
